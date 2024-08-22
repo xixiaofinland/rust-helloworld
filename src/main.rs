c35ae62c0b0a286152e05127f0ce8de1989aacaf
xi xiao <tdxiaoxi2@gmail.com>
2024-08-22 21:57:57 +0300

    + fixed by other's help

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub struct Class<'a> {
-    inner: &'a Node<'a>,
+pub struct Class<'a, 'tree> {
+    inner: &'a Node<'tree>,
 }
 
-impl<'a> Class<'a> {
-    pub fn get_1(&self) -> Vec<Node> {
+impl<'a, 'tree> Class<'a, 'tree> {
+    pub fn get_1(&self) -> Vec<Node<'tree>> {
         if let Some(n) = self.inner.get_child("modifiers") {
             vec![n]
         } else {
@@ -17,7 +17,7 @@ impl<'a> Class<'a> {
         }
     }
 
-    pub fn get_2(&'a self) -> Vec<Node> {
+    pub fn get_2(&self) -> Vec<Node<'tree>> {
         if let Some(n) = self.inner.get_child("modifiers") {
             n.get_children("modifier")
         } else {
@@ -26,13 +26,13 @@ impl<'a> Class<'a> {
     }
 }
 
-pub trait NodeUtilities {
-    fn get_child(&self, kind: &str) -> Option<Node>;
-    fn get_children(&self, kind: &str) -> Vec<Node>;
+pub trait NodeUtilities<'tree> {
+    fn get_child(&self, kind: &str) -> Option<Node<'tree>>;
+    fn get_children(&self, kind: &str) -> Vec<Node<'tree>>;
 }
 
-impl NodeUtilities for Node<'_> {
-    fn get_child(&self, kind: &str) -> Option<Node> {
+impl<'tree> NodeUtilities<'tree> for Node<'tree> {
+    fn get_child(&self, kind: &str) -> Option<Node<'tree>> {
         let mut cursor = self.walk();
         for child in self.children(&mut cursor) {
             if child.kind() == kind {
@@ -42,7 +42,7 @@ impl NodeUtilities for Node<'_> {
         None
     }
 
-    fn get_children(&self, kind: &str) -> Vec<Node> {
+    fn get_children(&self, kind: &str) -> Vec<Node<'tree>> {
         let mut cursor = self.walk();
         let mut modifiers = Vec::new();
         for child in self.children(&mut cursor) {
