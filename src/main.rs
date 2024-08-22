08f76e92a8488384e447c29b52a06fbdcb20895a
xi xiao <tdxiaoxi2@gmail.com>
2024-08-22 20:47:59 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use crate::utility::NodeUtilities;
 use tree_sitter::Node;
-
-mod utility;
 
 fn main() {
     println!("Hello, world!");
@@ -26,5 +23,33 @@ impl<'a> Class<'a> {
         } else {
             Vec::new()
         }
+    }
+}
+
+pub trait NodeUtilities {
+    fn get_child_by_kind(&self, kind: &str) -> Option<Node>;
+    fn get_children_by_kind(&self, kind: &str) -> Vec<Node>;
+}
+
+impl NodeUtilities for Node<'_> {
+    fn get_child_by_kind(&self, kind: &str) -> Option<Node> {
+        let mut cursor = self.walk();
+        for child in self.children(&mut cursor) {
+            if child.kind() == kind {
+                return Some(child);
+            }
+        }
+        None
+    }
+
+    fn get_children_by_kind(&self, kind: &str) -> Vec<Node> {
+        let mut cursor = self.walk();
+        let mut modifiers = Vec::new();
+        for child in self.children(&mut cursor) {
+            if child.kind() == kind {
+                modifiers.push(child);
+            }
+        }
+        modifiers
     }
 }
